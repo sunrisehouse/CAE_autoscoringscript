6263a55793985b69f3877d5705c2984a70a136cc
한정우 사원/AA Cloud Service 2팀 <jungwoooo.han@lgcns.com>
2023-06-09 17:03:35 +0900

    feat:script mission 2-1 and mission 2-2

--- score_mission2-1.sh
+++ score_mission2-1.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 podyamls=$(ls | grep -E "pod_[0-9]+\.yaml")
-log_dir="scoring_logs_mission2-1"
+log_dir="logs_mission2-1"
 result_path="result_mission2-1.txt"
-
-mkdir -p "$log_dir"
 
 ###############################################################################
 # Function
 ###############################################################################
 
-initialize_answer_env() {
+function initialize_answer_env {
   kubectl apply -f $1
-  sleep 100
+  sleep 20 
 }
 
-is_pass() {
+function is_pass {
   kubectl logs trouble > $1
-  local complete_count=$(grep -c 'Mission Complete!!' "$1")
-  if [$complete_count = 1 ]]; then
-    return true
+  local complete_count=$(grep -c "Mission Complete" $1)
+  if [[ $complete_count -eq 1 ]]
+  then
+    return 1 
   else
-    return false
+    return 0 
   fi
 }
 
-delete_answer_env() {
+function delete_answer_env {
   kubectl delete -f $1
 }
 
@@ -33,16 +32,22 @@ delete_answer_env() {
 # Main
 ###############################################################################
 
+mkdir -p "$log_dir"
+echo "[`date +%Y%m%d_%T`]" >> $result_path
+
 for podyaml in $podyamls; do
   number=$(echo $podyaml | grep -o -P '(?<=pod_)[0-9]+' | head -1)
   echo "[$number]"
 
   initialize_answer_env $podyaml
 
-  if [$( is_pass "$log_dir/$number.txt" )]; then
-    echo "[$number] O" >> $result_path
+  is_pass "$log_dir/$number.txt"
+  result=$?
+  if [[ $result -eq 1 ]]
+  then
+    echo "  - $number (O)" >> $result_path
   else
-    echo "[$number] X" >> $result_path
+    echo "  - $number (X)" >> $result_path
   fi
 
   delete_answer_env $podyaml
--- score_mission2-2.sh
+++ score_mission2-2.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 mutatingwebhookyamls=$(ls | grep -E "mutatingwebhook_[0-9]+\.yaml")
-log_dir="scoring_logs_mission2-2"
+log_dir="logs_mission2-2"
 result_path="result_mission2-2.txt"
-
-mkdir -p "$log_dir"
-kubectl apply -f "webhook.yaml"
 
 ###############################################################################
 # Function
@@ -13,20 +10,23 @@ kubectl apply -f "webhook.yaml"
 
 initialize_answer_env() {
   kubectl apply -f "$1"
-  kubectl run mynginx--image nginx--restart Never
+  kubectl run mynginx --image nginx--restart Never
+  sleep 20
 }
 
 is_pass() {
   kubectl get pods --show-labels > $1
-  local count=$(grep -c 'mutated=true' '$1')
-  if [count -eq 1]; then
-    return true
+  local count=$(grep -c "mutated=true" $1)
+  if [[ count -eq 1 ]]
+  then
+    return 1
   else
-    return false
+    return 0
   fi
 }
 
 delete_answer_env() {
+  kubectl delete pods mynginx
   kubectl delete -f "$1"
 }
 
@@ -34,16 +34,50 @@ delete_answer_env() {
 # Main
 ###############################################################################
 
+mkdir -p "$log_dir"
+cat <<EOT > webhook.yaml
+kind: Service
+apiVersion: v1
+metadata:
+  name: webhook
+  namespace: default
+spec:
+  selector:
+    app: webhook
+  ports:
+  - name: https
+    protocol: TCP
+    port: 443
+    targetPort: 5000
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: webhook
+  labels:
+    app: webhook
+spec:
+  containers:
+  - name: webhook
+    image: cnsedu/mutating:1.0
+EOT
+kubectl apply -f "webhook.yaml"
+
+echo "[`date +%Y%m%d_%T`]" >> $result_path
+
 for mutatingwebhookyaml in $mutatingwebhookyamls; do
   number=$(echo $mutatingwebhookyaml | grep -o -P '(?<=mutatingwebhook_)[0-9]+' | head -1)
   echo "[$number]"
 
   initialize_answer_env $mutatingwebhookyaml
-
-  if [$( is_pass "$log_dir/$number.txt" ) = true]; then
-    echo "[$number] O" >> $result_path
+  
+  is_pass "$log_dir/$number.txt"
+  result=$?
+  if [[ $result -eq 1 ]]
+  then
+    echo "  - $number (O)" >> $result_path
   else
-    echo "[$number] X" >> $result_path
+    echo "  - $number (X)" >> $result_path
   fi
 
   delete_answer_env $mutatingwebhookyaml
